a5ed8b6553e9f6905330d61df869037ac97940ff
gizmore <gizmore@wechall.net>
2023-03-12 21:38:43 +0100

    Init

---
+++ GDO_ChatMemory.php
@@ -0,0 +1,19 @@
+<?php
+namespace GDO\ChatGPT;
+
+use GDO\Core\GDO;
+
+/**
+ * A chatgpt file to train on.
+ * 
+ * @author gizmore
+ * @version 7.0.2
+ */
+final class GDO_ChatMemory extends GDO
+{
+	public function gdoColumns(): array
+	{
+		return [];
+	}
+
+}
---
+++ Module_ChatGPT.php
@@ -0,0 +1,60 @@
+<?php
+namespace GDO\ChatGPT;
+
+use GDO\Core\GDO_Module;
+use GDO\Core\GDT_Secret;
+use GDO\Core\WithComposer;
+
+/**
+ * ChatGPT bindings.
+ * 
+ * @author gizmore
+ * @version 7.0.2
+ */
+final class Module_ChatGPT extends GDO_Module
+{
+	
+	use WithComposer;
+	
+	public function getDependencies(): array
+	{
+		return [
+			'File',
+		];
+	}
+	
+	public function getClasses(): array
+	{
+		return [
+			GDO_ChatMemory::class,
+		];
+	}
+	
+	public function getConfig(): array
+	{
+		return [
+			GDT_Secret::make('chatgpt_apikey'),
+		];
+	}
+	
+	public function cfgApiKey(): ?string
+	{
+		return $this->getConfigVar('chatgpt_apikey');
+	}
+	
+	public function onLoadLanguage(): void
+	{
+		$this->loadLanguage('lang/chatgpt');
+	}
+	
+	public function onInstall(): void
+	{
+		$path = $this->filePath('secret.php');
+		$apikey = @include($path);
+		if ($apikey)
+		{
+			$this->saveConfigVar('chatgpt_apikey', $apikey);
+		}
+	}
+	
+}
