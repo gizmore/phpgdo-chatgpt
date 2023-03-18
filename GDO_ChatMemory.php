8d60ebab8be9fc19ab2b1905c06cf49241c19af9
gizmore <gizmore@wechall.net>
2023-03-18 23:19:53 +0100

    '7.0.1-r1763'

--- GDO_ChatMemory.php
+++ GDO_ChatMemory.php
@@ -2,6 +2,7 @@
 namespace GDO\ChatGPT;
 
 use GDO\Core\GDO;
+use GDO\Core\GDT_Name;
 use GDO\File\GDT_File;
 
 /**
@@ -15,7 +16,8 @@ final class GDO_ChatMemory extends GDO
 	public function gdoColumns(): array
 	{
 		return [
-			GDT_File::make()
+			GDT_Name::make('cm_name')->primary(),
+			GDT_File::make('cm_file')->notNull(),
 		];
 	}
 
